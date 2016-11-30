4c1eb5f3802f28e8ac5f79500639a68edfcd8506
cssmagic <cssmagic.cn@gmail.com>
2016-11-30 17:58:11 +0800

    Update building system.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 const path = require('path')
 const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
 
 const PATH_CSS = './src/css/'
 const FILES_CSS = './src/css/*.styl'
+const FILES_HTML = './src/*.html'
 
 gulp.task('css', () => {
 	const stylus = require('gulp-stylus')
@@ -32,8 +34,29 @@ gulp.task('css', () => {
 			}),
 		]))
 		.pipe(gulp.dest(PATH_CSS))
+		.pipe(browserSync.stream())
+})
+
+gulp.task('watch-css', () => {
+	gulp.watch(FILES_CSS, gulp.series('css'))
+})
+gulp.task('watch-html', () => {
+	gulp.watch(FILES_HTML)
+		.on('change', browserSync.reload)
+})
+
+gulp.task('serve', (done) => {
+    browserSync.init({
+        server: './src/',
+    })
+	done()
 })
 
 gulp.task('default', gulp.series([
-	'watch',
+	'css',
+	'serve',
+	gulp.parallel([
+		'watch-css',
+		'watch-html',
+	]),
 ]))
